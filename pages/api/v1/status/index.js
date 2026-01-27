e6ac6539844bea9d0b70a6b2c2cfb2f9d5704df7
Alex Cyrillo <alex.csborges@gmail.com>
2026-01-27 15:28:52 +0000

    Finalizar endpoint de `status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,27 @@
 import database from "/infra/database.js"
 
-function status(req, res) {
-  res.status(200).json({chave:"valor"})
+async function status(req, res) {
+
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbName = process.env.POSTGRES_DB;
+  const dbActiveConnections = await database.query({text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName]
+  });
+
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = dbVersion.rows[0].server_version;
+  const maxConnections = parseInt(dbMaxConnections.rows[0].max_connections);
+  const activeConnections = parseInt(dbActiveConnections.rows[0].count);
+
+  res.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: postgresVersion,
+          max_connections: maxConnections,
+          active_connections: activeConnections
+  }}})
 }
 
 export default status;
