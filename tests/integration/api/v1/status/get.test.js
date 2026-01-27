7e6939a327a79c6619e818c413f6447518ae7418
Alex Cyrillo <alex.csborges@gmail.com>
2026-01-27 15:30:22 +0000

    Aprimorar teste de status da API para verificar propriedades do corpo da resposta

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to api/v1/status should return 200", async () => {
-  res = await fetch("http://localhost:3000/api/v1/status")
+  const res = await fetch("http://localhost:3000/api/v1/status")
+  const resBody = await res.json();
+  const parsedUpdatedAt = new Date(resBody.updated_at).toISOString();
+
   expect(res.status).toBe(200);
+
+  expect(resBody.updated_at).toBe(parsedUpdatedAt);
+
+  expect(resBody.dependencies.database.version).toMatch(/^\d+\.\d+(\.\d+)?/)
+
+  expect(resBody.dependencies.database.max_connections).toBeGreaterThanOrEqual(0)
+
+  expect(resBody.dependencies.database.active_connections).toBeGreaterThanOrEqual(0)
+  
+  expect(resBody.dependencies.database.active_connections).toBe(1);
+
 })
